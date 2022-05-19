0fa6f001c20fb1a29ac8ab3375a54ad48c1d2805
Li Hui <lihuicham@gmail.com>
2022-05-19 18:30:39 +0800

    Setup track my day app file

---
+++ TRACK_MY_DAY/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Let's Start Working On Our App !</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffc0cb',
+    /*backgroundColor: '#fff',*/
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
