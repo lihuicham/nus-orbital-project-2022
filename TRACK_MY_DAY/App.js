7a36b955115cfcfaa32c08da11e282de3288d97c
pwintthiri <95198407+pwintthiri@users.noreply.github.com>
2022-05-21 23:59:24 +0800

    Revert "21 may changes"

--- TRACK_MY_DAY/App.js
+++ TRACK_MY_DAY/App.js
@@ -6,7 +6,6 @@ import ForgotPassword from './screens/ForgotPassword';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
